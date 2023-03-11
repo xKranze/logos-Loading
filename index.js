77d0a55a0257a806fc0d5efe2d561d2d70605764
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-03-11 10:54:21 -0800

    changed from questions to inquirer meth

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+//function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
         err ? console.log(err) : console.log('Successfully created your own logo!!')
     );
 };
 
-// TODO: Create a function to initialize app
+//function to initialize app
 function init() {
     inquirer.prompt(questions).then((response) =>
         writeToFile('./README.md', generateMarkdown({ ...response }))
