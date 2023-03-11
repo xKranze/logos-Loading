43a48f3c01cf1fd71f36bf31478e2152441cd220
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-03-10 22:42:44 -0800

    started shapes.js classes

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 
 //array of questions for user input
-const question = [
+const questions = [
     //logo text here up to 3 chars
     {
         type: 'input',
         name: 'text',
         message: 'Enter text for your logo here. (up to 3 characters)',
     },
-    //text color here
+    //text color here, color keyword or hexidecimal number.
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textcColor',
         message: 'Choose the color you want the text to be.',
     },
     //choose a shape choices circle,triangle, or square.
     {
         type: 'input',
-        name: 'shape-choice',
+        name: 'shapeChoice',
         message: 'Choose one of the following shapes.',
         choices:[
             'circle',
@@ -30,8 +30,24 @@ const question = [
     //color of the shape here.
     {
         type: 'input',
-        name: 'shape-color',
+        name: 'shapeColor',
         message: 'What color would you like your shape to be?',
     },
 ];
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created your own logo!!')
+    );
+};
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((response) =>
+        writeToFile('./README.md', generateMarkdown({ ...response }))
+    );
+}
+
+// Function call to initialize app
+init();
